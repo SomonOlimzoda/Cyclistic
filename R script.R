0d88561a279a3c9313994796f1421a2e9df87856
SomonTheAnalyst <somon.olimzoda1@gmail.com>
2023-01-02 17:17:39 +0900

    Update R script.R

--- R script.R
+++ R script.R
@@ -1,6 +1,22 @@
 
 ## Analyze ##
-##preparing stage of the analysis process (Setting up my workenvironment on R and uploading the dataset)  ##
+The purpose of this script is to consolidate downloaded Divvy data into a single data frame and then conduct simple analysis to help answer the key question:
+“In what ways do members and casual riders use Divvy bikes differently?”
+
+##  (Setting up my work environment on R and upload the dataset)  ##
+
+# Load packages
+Install required packages
+tidyverse for data import and wrangling
+lubridate for date functions
+ggplot for visualization
+
+library(tidyverse)  #helps wrangle data
+library(lubridate)  #helps wrangle date attributes
+library(ggplot2)  #helps visualize data
+getwd() #displays your working directory
+setwd("/Users/kevinhartman/Desktop/Divvy_Exercise/csv") #sets your working directory to simplify calls to data ... make sure to use your OWN username instead of mine ;)
+
 
 
 
